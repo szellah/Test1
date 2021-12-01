f4104f2a5346f52d9c8fa7fa69b6ed233014f945
SebastianZellah <s.zellah10@gmail.com>
2021-12-01 17:44:44 +0100

    Created test main

---
+++ src/test/java/SimpleWzimBoundedQueueTest.java
@@ -0,0 +1,36 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleWzimBoundedQueueTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void offer() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void remove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void poll() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void element() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void peek() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void size() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void capacity() {
+    }
+}
